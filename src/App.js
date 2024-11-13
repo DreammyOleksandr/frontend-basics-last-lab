446e9758a98a8c7134ce8a6b7f160c87fee7f195
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-13 18:22:22 +0200

    feat: main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import { Navbar } from './components/layout'
+import { MainPage } from './pages'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <MainPage />
     </div>
   )
 }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GetStartedBlock } from '../components/content'
+
+const MainPage = () => {
+  return (
+    <div>
+      <GetStartedBlock />
+    </div>
+  )
+}
+
+export default MainPage
